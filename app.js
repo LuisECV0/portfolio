3a10b88b3c618e289c1becc67f72fb05ae202e19
Luis <141294557+LuisECV0@users.noreply.github.com>
2023-09-18 23:43:33 -0500

    se implemento el envio de correos mediante la pag

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const nodemailer = require('nodemailer');
+
+app.use(express.json());
+app.use(cors());
+
+// Configuración de transporte de nodemailer
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  auth: {
+    user: 'luiscv1719@gmail.com',
+    pass: 'szvq-qkeh-mgqr-dqkz'
+  }
+});
+
+app.post('/enviar-correo', async (req, res) => {
+  const { nombres, correo, mensaje } = req.body;
+
+  if (!nombres) {
+    return res.status(400).json({ success: false, message: 'Debe proporcionar un nombre' });
+  }
+
+  if (!correo) {
+    return res.status(400).json({ success: false, message: 'Debe proporcionar un correo' });
+  }
+
+  if (!mensaje) {
+    return res.status(400).json({ success: false, message: 'Debe proporcionar un mensaje' });
+  }
+
+  const texto = `${nombres}\n${correo}\n${mensaje}`;
+
+  const mensajeCorreo = {
+    from: 'luiscv1719@gmail.com',
+    to: correo,
+    subject: 'Mensaje de contacto',
+    text: texto
+  };
+
+  try {
+    await transporter.sendMail(mensajeCorreo);
+    console.log('Correo enviado con éxito');
+    res.status(200).json({ success: true, message: 'Correo enviado con éxito' });
+  } catch (error) {
+    console.error('Error al enviar el correo:', error);
+    res.status(500).json({ success: false, message: 'Error al enviar el correo' });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`El servidor se encuentra corriendo en el puerto: ${PORT}`);
+});
--- javascript.js
+++ javascript.js
@@ -2,21 +2,16 @@ function toggleChat() {
     var chatbot = document.getElementById("chatbot");
     chatbot.style.display = chatbot.style.display === "none" ? "block" : "none";
 }
-
 function sendMessage() {
     var userInput = document.getElementById("user-input");
     var message = userInput.value;
     var chatMessages = document.getElementById("chat-messages");
-
     // Agrega el mensaje del usuario al chat
     chatMessages.innerHTML += '<div class="user-message">' + message + '</div>';
-    
     // Lógica para buscar una respuesta en función de la pregunta
     var respuesta = obtenerRespuesta(message);
-
     // Muestra la respuesta en el chat
     chatMessages.innerHTML += '<div class="bot-message">' + respuesta + '</div>';
-    
     // Limpia el cuadro de entrada
     userInput.value = "";
 }
@@ -28,7 +23,10 @@ function obtenerRespuesta(pregunta) {
         return "La Revolución Industrial fue un período de transformación económica y social en el siglo XIX...";
     } else if (pregunta.includes("Renacimiento y humanismo")) {
         return "El Renacimiento y el humanismo estaban estrechamente relacionados...";
-    } else {
+    } else if(pregunta.includes("Re zero")) {
+        return "es el besto anime isekai"
+    }
+    else {
         return "Lo siento, no tengo información sobre esa pregunta en este momento.";
     }
 }
